17fcd459ee364a3e33eb322d560c54912dea3951
XoloDream <Poisonshaft@gmail.com>
2024-04-29 19:27:15 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,15 +1,3 @@
---[[
-258258996, --[EASTER] Miner's Haven â›ï¸
-1778064565, --Restore Data 2
-4464946645, --The Void
-4780479031, --Revenge of John Doe
-4888384971, --Illusion
-5621678877, --Heart of Void
-5621679766, --The Temple
-5621680266, --Shiny Void
-16433781330, --Data Restore V3
---]]
-
 if game.PlaceId == 258258996 then -- Miner's Haven
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XoloDream/Roblox/main/MH%20-%20Secret.lua"))()
 elseif game.PlaceId == 4780479031 then -- Revenge of John Doe
