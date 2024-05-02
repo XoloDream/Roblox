790e7821fa6e06b7e941f7f0afe8ecaab437d893
XoloDream <Poisonshaft@gmail.com>
2024-05-02 12:56:31 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 258258996 then -- Miner's Haven
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XoloDream/Roblox/main/MH%20-%20Secret.lua"))()
 elseif game.PlaceId == 4780479031 then -- Revenge of John Doe
     loadstring(game:HttpGet("https://raw.githubusercontent.com/XoloDream/Roblox/main/MH%20-%20RoJD.lua"))()
+elseif game.PlaceId == 4464946645 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XoloDream/Roblox/main/MH%20-%20TV.lua"))()
 end
